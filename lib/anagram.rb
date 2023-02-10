97a0b23115c8483d6c84d733f06a81d57f2e1e6c
ntwigamartin <ntwiga.martin@gmail.com>
2023-02-10 17:28:50 +0300

    Completed, all tests passing

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words_array)
+        matched_array = []
+        words_array.select do |element|
+            if @word.chars.sort == element.chars.sort
+                matched_array.push(element)
+            end
+        end
+        matched_array
+    end
+end
+
+listen = Anagram.new('allergy')
+
+pp listen.match(%w[gallery ballerina regally clergy largely leading])
